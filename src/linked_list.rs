3b6268d46887697f28d390945b492e1a1de9ceee
DonizeteVida <donizetevida1998@outlook.com>
2024-12-09 04:03:10 -0300

    Basic Box based linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,3 +1,27 @@
 pub fn hello_world() {
     println!("Hello WOrld");
 }
+
+#[derive(Default, Debug)]
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn push(&mut self, item: T) {
+        let prev = std::mem::replace(&mut self.head, None);
+        self.head = Some(Box::new(Node::new(item, prev)));
+    }
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    item: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(item: T, next: Option<Box<Node<T>>>) -> Self {
+        Self { item, next }
+    }
+}
