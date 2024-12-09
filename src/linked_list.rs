fe615a18c8715f1717c146255a0963db48a88a68
DonizeteVida <donizetevida1998@outlook.com>
2024-12-09 04:16:42 -0300

    pop method and some tests

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,7 +1,3 @@
-pub fn hello_world() {
-    println!("Hello WOrld");
-}
-
 #[derive(Default, Debug)]
 pub struct LinkedList<T> {
     head: Option<Box<Node<T>>>,
@@ -12,16 +8,43 @@ impl<T> LinkedList<T> {
         let prev = std::mem::replace(&mut self.head, None);
         self.head = Some(Box::new(Node::new(item, prev)));
     }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match std::mem::replace(&mut self.head, None) {
+            None => None,
+            Some(item) => {
+                self.head = item.next;
+                Some(item.value)
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Node<T> {
-    item: T,
+    value: T,
     next: Option<Box<Node<T>>>,
 }
 
 impl<T> Node<T> {
-    fn new(item: T, next: Option<Box<Node<T>>>) -> Self {
-        Self { item, next }
+    fn new(value: T, next: Option<Box<Node<T>>>) -> Self {
+        Self { value, next }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_add_and_pop() {
+        let mut list = LinkedList::<u32>::default();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 mod linked_list;
 
 fn main() {
-    linked_list::hello_world();
-
-    let mut list = linked_list::LinkedList::<u32>::default();
-    list.push(1);
-    list.push(2);
-    list.push(3);
-    println!("{:?}", list);
+    
 }
